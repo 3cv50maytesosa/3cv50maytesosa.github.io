fba5d5d2a88d52d4f15a301269b6caeb6351282e
3cv50maytesosa <84733525+3cv50maytesosa@users.noreply.github.com>
2021-05-24 12:55:03 -0500

    Create ctrlT1.js

---
+++ js/ctrlT1.js
@@ -0,0 +1,21 @@
+"use strict";
+var forma = document.getElementById("forma"),
+    salidaBoleta = document.getElementById("salidaBoleta"),
+    salidaNombre = document.getElementById("salidaNombre"),
+    salidaGrupo = document.getElementById("salidaGrupo"),
+    salidaMateria = document.getElementById("salidaMateria"),
+    txtFecha = forma["fecha"], 
+    salidaFecha = document.getElementById("salidaFecha"),
+    salidaDatos = document.getElementById("salidaDatos");
+
+function captura() {
+    
+    var fecha = new Date(txtFecha.value),
+    boleta = forma["boleta"].value.trim(),
+    nombre = forma["nombre"].value.trim(),
+    grupo = forma["grupo"].value.trim(),
+    materia = forma["materia"].value.trim();
+
+    salidaDatos.textContent = "Boleta: " + boleta + "    |    " + " Nombre " + nombre + "    |    " + " Grupo:" + grupo 
+    + "    |    " + "Materia: " + materia + "    |    " + " Fecha: " + fecha.toUTCString();
+}
